5e068bf65709f5eb8320c93e9c301b20b34c66c5
Jess Astbury <jess.astbury@hotmail.com>
2017-02-19 18:02:03 +0000

    Adds author and year to Book

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -5,12 +5,24 @@ package com.twu.biblioteca;
  */
 public class Book {
     String title;
+    String author;
+    int year;
 
-    public Book(String bookTitle) {
+    public Book(String bookTitle, String bookAuthor, int bookYear) {
         title = bookTitle;
+        author = bookAuthor;
+        year = bookYear;
     }
 
     public String getTitle() {
         return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,11 +9,26 @@ import static org.junit.Assert.assertEquals;
  * Created by jessastbury on 19/02/2017.
  */
 public class BookTest {
+    Book b;
+
+    @Before
+    public void init() {
+        b = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925);
+    }
 
     @Test
-    public void newBook() {
-        Book b = new Book("The Great Gatsby");
+    public void bookTitle() {
         assertEquals("The Great Gatsby", b.getTitle());
+    }
+
+    @Test
+    public void bookAuthor() {
+        assertEquals("F. Scott Fitzgerald", b.getAuthor());
+    }
+
+    @Test
+    public void bookYear() {
+        assertEquals(1925, b.getYear());
     }
 
 }
